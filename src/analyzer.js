c9b99bb56facb324cb75977e3231237d9a49a03a
Diana <educ.dana@gmail.com>
2023-12-31 00:50:54 -0500

    metricas completas solo existen algunos errores en el test

--- src/analyzer.js
+++ src/analyzer.js
@@ -4,6 +4,8 @@ const analyzer = {
     const palabras = text.split(/\s+/);
     const palabrasFiltradas = palabras.filter(palabra => palabra !== '');
     return palabrasFiltradas.length;
+
+    
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
@@ -18,26 +20,64 @@ const analyzer = {
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
     
-    
+    const cantDeCaracteres = text.replace(/\s/g, "").length;
+    // const cantDeCaracteres = analyzer.getCharacterCountExcludingSpaces(text);  /*para llamar a otra funcion, en este caso debe ir primero analyzer.*/
+    const cantPalabras = analyzer.getWordCount(text); /*para llamar a otra funcion, en este caso debe ir primero analyzer.*/
+
+    const promedioPalabras = (cantDeCaracteres / cantPalabras).toFixed(2) ;  /*.toFixed(2)..(numero): este metodo sirve para que solo aparezcan 2 decimales*/
+
+    // console.log('Promedio:',promedioPalabras);
+
+    return promedioPalabras;
+
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-    // SI POR EJEMPLO 90 SON 2 NUMEROS O UNO
-    // const cantnumeros = text.replace(/[\D]+/g,'').length;   
-    // Encontrar coincidencias de números en el texto
-  const cantnumeros = text.match(/\d+/g);
+    // 
+    const cantnumeros = text.match(/\b\d+(\.\d+)?\b/g);  /* match:especifica la coincidencia de una expresión regular con una cadena*/
 
-  // Contar el número total de números encontrados
-  const contadorNumeros = cantnumeros ? cantnumeros.length : 0;
+    // Contar el número total de números encontrados
+    const contadorNumeros = cantnumeros ? cantnumeros.length : 0;
 
-  // Mostrar el resultado en la consola o en otro lugar según tus necesidades
-  console.log('Números:', contadorNumeros);   
-  return contadorNumeros;  
+    // console.log('Números:', contadorNumeros);   
+
+    return contadorNumeros; 
     
-},
+    // const soloNumeros = text.replace(/\D/g, ''); // \D coincide con caracteres que no son dígitos
+    
+    // console.log(soloNumeros); // Esto mostrará "123"
+
+    // Mostrar el resultado en la consola o en otro lugar según tus necesidades
+    // console.log('Números:', contadorNumeros);   
+    
+    
+  },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-    
+    const cantNumeros = text.match(/\b\d+(\.\d+)?\b/g);
+
+    // Verifica si hay números
+    if (!cantNumeros) {
+      // console.log('No se encontraron números.');
+      return 0;
+    }
+  
+    // Inicializa la suma
+    let sumaNumeros = 0;
+  
+    // Itera sobre cada número y agrégalo a la suma
+    for (let i = 0; i < cantNumeros.length; i++) {
+      // Convierte cada número de cadena a flotante y suma
+      sumaNumeros += parseFloat(cantNumeros[i]);
+    }
+  
+    // Limita la suma a dos decimales
+    sumaNumeros = parseFloat(sumaNumeros.toFixed(2));
+  
+    // console.log('Suma de números:', sumaNumeros);
+  
+    return sumaNumeros;
+  
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -4,92 +4,88 @@ import analyzer from './analyzer.js';
 
 // evento reset-button
 document.addEventListener("DOMContentLoaded", function() {
-    const textarea =document.querySelector('[name="cuadro"]');
-    const resetboton = document.getElementById("reset-button");
-    
-    resetboton.addEventListener("click",function(){
-         textarea.value =" ";
-    });
+  const textarea = document.querySelector('[name="user-input"]');
+  const resetboton = document.getElementById("reset-button");
+
+  resetboton.addEventListener("click", function () {
+    textarea.value = " ";
+  });
 
 });
 
 // QUE LA INFORMACION ESTE EN LA CONSOLA/CONSOLE
-document.addEventListener("DOMContentLoaded", function () {
-    const textarea =document.querySelector('[name="cuadro"]');
-    const contadorPalabras = document.querySelector('.metrica[data-testid="word-count"]')
+// document.addEventListener("DOMContentLoaded", function () {
+//   const textarea = document.querySelector('[name="user-input"]');
+//   const contadorPalabras = document.querySelector('.metrica[data-testid="word-count"]')
 
-textarea.addEventListener("input",function(e){
-   console.log(e.target.value)
-})
-});
+//   textarea.addEventListener("input", function (e) {
+//     console.log(e.target.value)
+//   })
+// });
+
 // CONTEO DE PALABRAS
-  // Obtener referencia al área de texto y al elemento de conteo
-    const inputTextarea = document.querySelector('[name="cuadro"]');
-    const contadorPalabras = document.querySelector('.metrica[data-testid="word-count"]');
+// Obtener referencia al área de texto y al elemento de conteo
+const inputTextarea = document.querySelector('[name="user-input"]');
+const contadorPalabras = document.querySelector('.metrica[data-testid="word-count"]');
 
-  // Agregar un evento de entrada al área de texto
-    inputTextarea.addEventListener("input", function() {
-    // Obtener el valor del texto ingresado
-    const textoIngresado = inputTextarea.value;
+// Agregar un evento de entrada al área de texto
+inputTextarea.addEventListener("input", function () {
+  // Obtener el valor del texto ingresado
+  const textoIngresado = inputTextarea.value;
 
-    // Obtener el número de palabras y actualizar el contador(no olvidar el analyzer.)
-    const numeroPalabras = analyzer.getWordCount(textoIngresado);
-    contadorPalabras.textContent = 'Recuento de palabras:'+ numeroPalabras; /*otra forma es con ${numeroPalabras}*/
-  });
+  // Obtener el número de palabras y actualizar el contador(no olvidar el analyzer.)
+  const numeroPalabras = analyzer.getWordCount(textoIngresado);
+  contadorPalabras.textContent = 'Recuento de palabras:' + numeroPalabras; /*otra forma es con ${numeroPalabras}*/
+});
 
 // CONTEO DE CARACTERES
-    const contadorCaracteres = document.querySelector('.metrica[data-testid="character-count"]');
+const contadorCaracteres = document.querySelector('.metrica[data-testid="character-count"]');
 
-    inputTextarea.addEventListener("input", function() {
-    const textoIngresado = inputTextarea.value;
-    
-    const Caracteres = analyzer.getCharacterCount(textoIngresado);
-    contadorCaracteres.textContent = 'Caracteres:' + Caracteres;
+inputTextarea.addEventListener("input", function () {
+  const textoIngresado = inputTextarea.value;
+
+  const Caracteres = analyzer.getCharacterCount(textoIngresado);
+  contadorCaracteres.textContent = 'Caracteres:' + Caracteres;
 });
 // CARACTERES SIN ESPACIOS,SIN SIGNOS DE PUNTUACION
-    const contadorCaracteresSin = document.querySelector('.metrica[data-testid="character-no-spaces-coun"]');
+const contadorCaracteresSin = document.querySelector('.metrica[data-testid="character-no-spaces-coun"]');
 
-    inputTextarea.addEventListener("input", function() {
-    const textoIngresado = inputTextarea.value;
-    
+inputTextarea.addEventListener("input", function () {
+  const textoIngresado = inputTextarea.value;
+
   const CaracteresSin = analyzer.getCharacterCountExcludingSpaces(textoIngresado);
-  contadorCaracteresSin.textContent = 'CaracteresCaracteres excluyendo espacios y signos de puntuación:' + CaracteresSin;
+  contadorCaracteresSin.textContent = 'Caracteres excluyendo espacios y signos de puntuación:' + CaracteresSin;
 });
 // //  CANT DE NUMEROS
-    const contadorcantNumeros = document.querySelector('.metrica[data-testid="number-count"]')
-    
-    inputTextarea.addEventListener("input", function() {
-        const textoIngresado = inputTextarea.value;
-    
-    const CantNumeros = analyzer.getNumberCount(textoIngresado);
-    contadorcantNumeros.textContent = 'Números:' + CantNumeros;
+const contadorcantNumeros = document.querySelector('.metrica[data-testid="number-count"]')
+
+inputTextarea.addEventListener("input", function () {
+  const textoIngresado = inputTextarea.value;
+
+  const CantNumeros = analyzer.getNumberCount(textoIngresado);
+  contadorcantNumeros.textContent = 'Números:' + CantNumeros;
 });
 
- 
+// LONGITUD MEDIA DE PALABRAS
+const Longitudmedia = document.querySelector('.metrica[data-testid="word-length-average"]')
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     // Obtener referencia al textarea y al botón
-//     var textarea = document.querySelector('[name="cuadro"]');
-//     var resetButton = document.getElementById('reset-button');
+inputTextarea.addEventListener("input", function () {
+  const textoIngresado = inputTextarea.value;
   
-//     // Agregar event listener al botón
-//     resetButton.addEventListener('click', function () {
-//       // Limpiar el contenido del textarea
-//       textarea.value = '';
-//     });
-//   });
+  const PromedioPalabras = analyzer.getAverageWordLength(textoIngresado);
+  Longitudmedia.textContent = 'Promedio de longitud de palabra:' + PromedioPalabras;
+});
 
-   
-// no entiendo por que va el cero y value.
-//   document.addEventListener('DOMContentLoaded', function () {
-//     var textarea = document.getElementsByName('cuadro')[0];
-//     var resetButton = document.getElementById('reset-button');
+// SUMA DE NUMEROS
+
+const Sumanumeros = document.querySelector('.metrica[data-testid="number-sum"]')
+
+inputTextarea.addEventListener("input", function () {
+  const textoIngresado = inputTextarea.value;
   
-//     resetButton.addEventListener('click', function () {
-//       // Limpiar el contenido del textarea
-//       textarea.value = '';
-//     });
-//   });
+  const SumadeNumeros = analyzer.getNumberSum(textoIngresado);
+  Sumanumeros.textContent = 'Suma de números:' + SumadeNumeros;
+});
 
 
 
